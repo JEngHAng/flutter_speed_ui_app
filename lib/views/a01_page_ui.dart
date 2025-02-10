55dd69f5327254f83b899f45c8db3d6b08684d14
JEngHAng <jangg500z@gmail.com>
2025-02-10 18:16:46 +0700

    A01_page finish

--- lib/views/a01_page_ui.dart
+++ lib/views/a01_page_ui.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: sort_child_properties_last
+
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_ui_app/views/a02_page_ui.dart';
 
 class A01PageUI extends StatefulWidget {
   const A01PageUI({super.key});
@@ -10,6 +13,143 @@ class A01PageUI extends StatefulWidget {
 class _A01PageUIState extends State<A01PageUI> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.15,
+            ),
+            Image.asset(
+              "assets/images/a/imga1.png",
+              width: MediaQuery.of(context).size.width * 0.7,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.1,
+            ),
+            Text (
+              "Discover Your",
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.height * 0.025,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            Text (
+              "Own Dream House",
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.height * 0.025,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.025,
+            ),
+            Text (
+              "Lorem ipsum dolor sit amet, consectetur adiscing elit.",
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.height * 0.015,
+                fontWeight: FontWeight.normal,
+                color: Colors.black,
+              ),
+            ),
+            Text (
+              "Diam maecenas mi non sed ut odio. Non,justo,sed facilisi",
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.height * 0.015,
+                fontWeight: FontWeight.normal,
+                color: Colors.black,
+              ),
+            ),
+            Text (
+              "et. Eget viverra urna, vestibulum egestas faucibus",
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.height * 0.015,
+                fontWeight: FontWeight.normal,
+                color: Colors.black,
+              ),
+            ),
+            Text (
+              "egestas. Sagittis nam velit volutpat eu nunc",
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.height * 0.015,
+                fontWeight: FontWeight.normal,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.1,
+            ),
+            Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => A02PageUI(),
+                    ),
+                  );
+                },
+                child: Text(
+                  "Sign in",
+                  style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.height * 0.025,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(
+                    162.5,
+                    50.0
+                  ),
+                  backgroundColor: const Color.fromARGB(255, 201, 68, 185),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      7.0,
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => A02PageUI(),
+                    ),
+                  );
+                },
+                child: Text(
+                  "Register",
+                  style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.height * 0.025,
+                    fontWeight: FontWeight.bold,
+                    color: const Color.fromARGB(255, 0, 0, 0),
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(
+                    162.5,
+                    50.0
+                  ),
+                  backgroundColor: const Color.fromARGB(255, 222, 222, 222),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      7.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          ],
+        ),
+      ),
+    );
   }
 }
