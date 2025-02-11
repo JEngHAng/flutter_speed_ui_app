0eb4de693ea0ac771581f496884925f6efb742c3
JEngHAng <jangg500z@gmail.com>
2025-02-12 01:29:37 +0700

    Page D06 Finish

--- lib/views/d06_page_ui.dart
+++ lib/views/d06_page_ui.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: sort_child_properties_last
+
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_ui_app/views/d07_page_ui.dart';
 
 class D06PageUI extends StatefulWidget {
   const D06PageUI({super.key});
@@ -10,6 +13,102 @@ class D06PageUI extends StatefulWidget {
 class _D06PageUIState extends State<D06PageUI> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 65,),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: const Color.fromARGB(86, 71, 71, 71), width: 2),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: IconButton(
+                  icon: const Icon(Icons.arrow_back, color: Colors.black),
+                  onPressed: () => Navigator.pop(context),
+                ),
+              ),
+
+              const SizedBox(height: 32),
+              const Text(
+                "Create new password",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 14),
+              const Text(
+                "Your new password must be unique frorm those\npreviously used",
+                style: TextStyle(color: Colors.grey, fontSize: 15),
+              ),
+              const SizedBox(height: 24),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "New Password",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey[200], // พื้นหลังช่องกรอก
+                ),
+              ),
+            
+              const SizedBox(height: 14),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Confirm Password",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey[200], // พื้นหลังช่องกรอก
+                ),
+              ),
+
+              SizedBox(height: 35),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => D07PageUI(),
+                      ),
+                    );
+                  },
+                    child: Text(
+                      "Reset Password",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: Size(
+                        325.0,
+                        50.0
+                      ),
+                      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                          7.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
